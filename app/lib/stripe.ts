f96bdafa7faab6ccd83f25efc69acd1a6f3c0bfd
maruf_hossain <marufhossain123098@gmail.com>
2025-10-01 16:34:22 +0600

    payment done

--- app/lib/stripe.ts
+++ app/lib/stripe.ts
@@ -4,8 +4,8 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error("STRIPE_SECRET_KEY is not defined");
 }
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-08-27.basil",
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-09-30.clover",
 });
 
 export default stripe;
